71f808e88c3c91e0aab470de6362f79332fda661
Pancholi <161453107+Pancholi22@users.noreply.github.com>
2025-12-11 11:56:31 +0530

    Create dynamsoft.service.config.js

---
+++ Resources/src/dynamsoft.service.config.js
@@ -0,0 +1,22 @@
+<script>
+Dynamsoft = window.Dynamsoft || {};
+Dynamsoft.DWT = Dynamsoft.DWT || {};
+
+Dynamsoft.DWT.ProductKey = "f0068ZQAAABvwZ61KmatKuSFLfk7lw/a7S1V+NVK1A18jG8OBy6/5kUgKdwBYAw3zmePXWq/Eidf5SfyS4HRCUsDmhb7H02M=";
+Dynamsoft.DWT.ResourcesPath = "Resources";
+
+// *** REQUIRED FOR SERVICE MODE ***
+Dynamsoft.DWT.UseLocalService = true;
+Dynamsoft.DWT.IfDetectDirectScanner = false; // Prevent Chrome local network popup
+Dynamsoft.DWT.UseCameraViaHTTP = false;
+
+// IMPORTANT: Tell WebTWAIN where the local Dynamsoft Service runs:
+Dynamsoft.DWT.ServicePort = 18626;  // default
+Dynamsoft.DWT.ServiceHost = "127.0.0.1";
+
+// Prevent iframe scanning restrictions
+Dynamsoft.DWT.IfInitDirectly = true;
+
+// Optional but recommended
+Dynamsoft.DWT.IfUseService = true;
+</script>
